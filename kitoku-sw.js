b569248f0daebca14448bfaa2f32f765769f983e
dawnanddusk6116-cmyk <dawnanddusk6116@gmail.com>
2026-07-27 15:15:36 +0900

    Add files via upload

--- kitoku-sw.js
+++ kitoku-sw.js
@@ -1,4 +1,4 @@
-const KITOKU_CACHE = 'kitoku-pwa-v6-report-owner-prefill-v20260727h';
+const KITOKU_CACHE = 'kitoku-pwa-v6-academy-font-v20260727k';
 const KITOKU_CORE = [
   './index.html',
   './top.html',
