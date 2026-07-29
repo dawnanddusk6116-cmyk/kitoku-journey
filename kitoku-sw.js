532911e201dfb2c9300740d9e4674176a285f61b
dawnanddusk6116-cmyk <dawnanddusk6116@gmail.com>
2026-07-29 12:16:35 +0900

    Add files via upload

--- kitoku-sw.js
+++ kitoku-sw.js
@@ -1,4 +1,4 @@
-const KITOKU_CACHE = 'kitoku-pwa-v6-academy06-shots-v20260729b';
+const KITOKU_CACHE = 'kitoku-pwa-v6-academy06-video-v20260729c';
 const KITOKU_CORE = [
   './index.html',
   './top.html',
