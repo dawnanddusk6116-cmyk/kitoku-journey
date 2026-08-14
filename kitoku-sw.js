ebc69134c02f6ad16e860ba2ca2e694d732dbdb5
dawnanddusk6116-cmyk <dawnanddusk6116@gmail.com>
2026-08-15 07:54:54 +0900

    Add files via upload

--- kitoku-sw.js
+++ kitoku-sw.js
@@ -1,4 +1,4 @@
-const KITOKU_CACHE = 'kitoku-pwa-v6-saved-places-v20260803t';
+const KITOKU_CACHE = 'kitoku-pwa-v6-saved-places-v20260803u';
 const KITOKU_CORE = [
   './index.html',
   './top.html',
