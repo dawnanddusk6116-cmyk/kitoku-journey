c6cbd6a5283542a28946962503dbb85d5e166578
dawnanddusk6116-cmyk <dawnanddusk6116@gmail.com>
2026-08-27 11:33:47 +0900

    Add files via upload

--- kitoku-sw.js
+++ kitoku-sw.js
@@ -1,4 +1,4 @@
-const KITOKU_CACHE = 'kitoku-pwa-v6-saved-places-v20260827-year-break-tendo-merge';
+const KITOKU_CACHE = 'kitoku-pwa-v6-saved-places-v20260827-map-year-55-label';
 const KITOKU_CORE = [
   './index.html',
   './top.html',
