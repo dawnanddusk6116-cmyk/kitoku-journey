5bd51575fd363c713b51528fcf58ec4e04f73a6e
dawnanddusk6116-cmyk <dawnanddusk6116@gmail.com>
2026-08-29 09:34:21 +0900

    Add files via upload

--- kitoku-sw.js
+++ kitoku-sw.js
@@ -1,4 +1,4 @@
-const KITOKU_CACHE = 'kitoku-pwa-v6-saved-places-v20260829-roadmap-rename';
+const KITOKU_CACHE = 'kitoku-pwa-v6-saved-places-v20260829-roadmap-integrated';
 const KITOKU_CORE = [
   './index.html',
   './top.html',
